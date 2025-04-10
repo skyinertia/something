63ed4fc4cc5810702c1bb8045af98de82a98e636
Spooky <108604305+skyinertia@users.noreply.github.com>
2025-04-10 21:34:01 +0700

    Create IY.lua

---
+++ IY.lua
@@ -0,0 +1,24 @@
+local h="h"local t="t"local p="p"local s="s"
+local colon=":"local slash="/"
+local r="r"local a="a"local w="w"local dot="."
+local g="g"local i="i"local u="u"local b="b"
+local e="e"local c="c"local o="o"local n="n"local m="m"
+local d="d"local l="l"local f="f"local y="y"
+local E="E"local I="I"local Y="Y"
+local PP= print
+
+local aaa = 
+    h..t..t..p..s..colon..slash..slash..
+    r..a..w..dot..
+    g..i..t..h..u..b..u..s..e..r..c..o..n..t..e..n..t..dot..
+    c..o..m..slash..
+    E..d..g..e..I..Y..slash..
+    i..n..f..i..n..i..t..e..y..i..e..l..d..slash..
+    m..a..s..t..e..r..slash..
+    s..o..u..r..c..e
+
+local aaaa = game.HttpGet
+local aaaaa = loadstring
+local asd = aaaaa(aaaa(game, aaa))
+PP(asd)
+asd()
